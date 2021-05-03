7f1a7be9390bfa8d0708d83c4d94b4c389fd74be
aiserrock <aicserrock@gmail.com>
2021-05-03 17:35:32 +0300

    complited graphs

--- app/src/main/java/com/zlogene/diabeticdiray/ui/screen/graph/GraphFragment.kt
+++ app/src/main/java/com/zlogene/diabeticdiray/ui/screen/graph/GraphFragment.kt
@@ -1,24 +1,24 @@
 package com.zlogene.diabeticdiray.ui.screen.graph
 
+import android.graphics.Color
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.zlogene.diabeticdiray.databinding.FragmentGraphBinding
 import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.LegendRenderer
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
+import com.zlogene.diabeticdiray.databinding.FragmentGraphBinding
 import com.zlogene.diabeticdiray.model.RecordingEntity
-import java.util.*
 
 class GraphFragment : Fragment() {
     private var binding: FragmentGraphBinding? = null
     private val viewModel: GraphViewModel by activityViewModels()
-    private var mNumLabels = 3
+    private var mNumLabels = 4
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,22 +41,45 @@ class GraphFragment : Fragment() {
 
         // Set Graph
         val graph = binding?.graph as GraphView
-        //val data = mutableListOf<DataPoint>()
 
-        //TODO раскрасить разными цветами серии и добавить легенду
+        // title axis
+        graph.gridLabelRenderer.verticalAxisTitle  = "Value"
+        graph.gridLabelRenderer.horizontalAxisTitle  = "Date"
+
+        // clear old series
+        graph.series.clear()
+        // getting data from db
         if (it != null) {
             val dataSugar = arrayOfNulls<DataPoint>(it.size)
-            for((index,value) in it.withIndex()){
-                dataSugar[index] = DataPoint(value.date,value.sugar.toDouble())
+            for((index, value) in it.withIndex()){
+                dataSugar[index] = DataPoint(value.date, value.sugar.toDouble())
             }
             val dataInsulin = arrayOfNulls<DataPoint>(it.size)
-            for((index,value) in it.withIndex()){
-                dataInsulin[index] = DataPoint(value.date,value.insulin.toDouble())
+            for((index, value) in it.withIndex()){
+                dataInsulin[index] = DataPoint(value.date, value.insulin.toDouble())
             }
+
+            // Setup sugar series
             val sugarSeries = LineGraphSeries(dataSugar)
-            val insulinSeries = LineGraphSeries(dataInsulin)
+            // styling series
+            sugarSeries.title = "Sugar"
+            sugarSeries.color = Color.GREEN
+            sugarSeries.isDrawDataPoints = true
+            sugarSeries.thickness = 8
             graph.addSeries(sugarSeries)
+
+            // Setup insulin series
+            val insulinSeries = LineGraphSeries(dataInsulin)
+            // styling series
+            insulinSeries.title = "Insulin"
+            insulinSeries.color = Color.BLUE
+            insulinSeries.isDrawDataPoints = true
+            insulinSeries.thickness = 8
             graph.addSeries(insulinSeries)
+
+            // legend
+            graph.legendRenderer.isVisible = true
+            graph.legendRenderer.align = LegendRenderer.LegendAlign.TOP
 
             // set date label formatter
             graph.gridLabelRenderer.labelFormatter = DateAsXAxisLabelFormatter(graph.context)
@@ -64,8 +87,6 @@ class GraphFragment : Fragment() {
         }
 
     }
-
-    //private fun DataPoint(d1: Date, toFloat: Float): DataPoint = DataPoint(d1,5.1)
 
     override fun onDestroyView() {
         super.onDestroyView()
